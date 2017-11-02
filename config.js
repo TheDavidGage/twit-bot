09ee0b1fe240cc63c1feec1e5a9754df8ecce2fe
David Gage Hall <FearlessDavidGage@TheDavidGage.local>
2017-11-02 15:33:36 -0400

    second commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'Csfz8KGCqGbqvFWQVLUe1MtZD',
+    consumer_secret: 'vGnxlvBdK1CJTpnHeEdkgczcynf3psy64EYW6gccSdizOpgvM4',
+    access_token_key: '3225636128-sxhV4grl05FpY9OQ1e8BeOuA5qHULYa7A7VPeo3',
+    access_token_secret: 'aVN8qSufdjbvnPTheNdU1XpUagIvI1PQh8o7qf36wfc8Q'
+  }
