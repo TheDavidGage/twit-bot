466b83c12cb4671d710c2ba57e389eaee95b07f7
David Gage Hall <FearlessDavidGage@TheDavidGage.local>
2017-11-04 15:06:22 -0400

    follow bot

---
+++ follow.js
@@ -0,0 +1,34 @@
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: '#Node.js',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+// Initiate your search using the above paramaters
+T.get('search/tweets', params, function(err, data, response) {
+  // If there is no error, proceed
+  if(!err){
+    // Loop through the returned tweets
+    for(let i = 0; i < data.statuses.length; i++){
+        let screen_name = data.statuses[i].user.screen_name;
+        T.post('friendships/create', {screen_name}, function(err, res){
+            if(err){
+              console.log(err);
+            } else {
+              console.log(screen_name, ': **FOLLOWED**');
+            }
+          });
+
+
+    }
+  } else {
+    console.log(err);
+  }
+})
